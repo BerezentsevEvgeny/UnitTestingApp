b2e9e2c5615fcceb59f7353ec3dca98dc984f988
BerezentesvEvgeny <jonn09@icloud.com>
2021-10-12 20:50:27 +0300

    Initial setup

---
+++ UnitTestingAppTests/DataModelTests.swift
@@ -0,0 +1,35 @@
+//
+//  UnitTestingAppTests.swift
+//  UnitTestingAppTests
+//
+//  Created by Евгений Березенцев on 12.10.2021.
+//
+
+import XCTest
+@testable import UnitTestingApp
+
+class DataModelTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+    }
+
+    override func tearDown() {
+        
+        
+        super.tearDown()
+    }
+
+    func testExample()  {
+
+    }
+
+    func testPerformanceExample() {
+        
+        self.measure {
+            
+        }
+    }
+
+}
